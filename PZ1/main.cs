571a883011c33e143f2e8b46d99c1c37a0f59648
Igoltend <150811402+Igoltend@users.noreply.github.com>
2024-03-28 14:40:59 +0200

    Update main.cs

--- PZ1/main.cs
+++ PZ1/main.cs
@@ -1,44 +1,6 @@
-// using System;
-// using System.Text.RegularExpressions;
-
-// record IP4(byte[] Address);
-
-// class Parser {
-//     private Regex regex = new Regex(@"(\s?[\.,!-?]\s?)");
-
-//     public IP4? Parse(string s) {
-//         Match match = regex.Match(s);
-//         if (!match.Success){
-//             return null;
-//         }
-
-//         return new IP4(new byte[]{
-//             byte.Parse(match.Groups[1].Value),
-//             byte.Parse(match.Groups[2].Value),
-//             byte.Parse(match.Groups[3].Value),
-//             byte.Parse(match.Groups[4].Value),
-//         });
-//     }
-// }
-
-// class Program {
-//     static void Main(string[] args) {
-//         string? s;
-//         Parser parser = new Parser();
-//         while ((s=Console.ReadLine()) != null)
-//         {
-//             IP4? ip = parser.Parse(s);
-//             if (ip == null){
-//                 Console.WriteLine($"Invalid IP address: {s}");
-//             } else
-//             {
-//                 Console.WriteLine($"Parsed IP address: {ip.Address[0]}.{ip.Address[1]}.{ip.Address[2]}.{ip.Address[3]}");
-//             }
-//         }
-//     }
-// }
 
 
+// 13 варіант
 
 using System;
 using System.Collections.Generic;
@@ -59,17 +21,17 @@ class Item {
 class Inventory {
     private List<Item> items = new List<Item>();
 
-    // Додавання речі до інвентаря
+    
     public void AddItem(Item item) {
         items.Add(item);
     }
 
-    // Видалення речі з інвентаря
+    
     public void RemoveItem(Item item) {
         items.Remove(item);
     }
 
-    // Повертає тип речей, яких доступно найбільше
+    
     public string MostCommonType() {
         if (items.Count == 0) {
             return "Інвентар порожній";
@@ -82,7 +44,7 @@ class Inventory {
         return typeCounts.First().Type;
     }
 
-    // Повертає тип речей, яких доступно найменше
+
     public string LeastCommonType() {
         if (items.Count == 0) {
             return "Інвентар порожній";
@@ -100,7 +62,7 @@ class Program {
     static void Main(string[] args) {
         Inventory inventory = new Inventory();
 
-        // Додавання речей до інвентаря
+        
         inventory.AddItem(new Item("Меч", "Зброя", 5.0));
         inventory.AddItem(new Item("Щит", "Захист", 10.0));
         inventory.AddItem(new Item("Зелье здоровья", "Предмет для відновлення", 1.0));
