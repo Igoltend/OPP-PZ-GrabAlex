d44a392b945a85c5114e281e6a215111daf4d7b5
Igoltend <150811402+Igoltend@users.noreply.github.com>
2024-04-29 08:17:07 +0300

    Add files via upload

---
+++ pz5/Pz5/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Pz5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxYear = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxBrand = new System.Windows.Forms.ComboBox();
+            this.comboBoxModel = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.radioButtonAutomatic = new System.Windows.Forms.RadioButton();
+            this.radioButtonManual = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxYear
+            // 
+            this.textBoxYear.Location = new System.Drawing.Point(183, 167);
+            this.textBoxYear.Name = "textBoxYear";
+            this.textBoxYear.Size = new System.Drawing.Size(100, 22);
+            this.textBoxYear.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 172);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Рік випкску";
+            // 
+            // comboBoxBrand
+            // 
+            this.comboBoxBrand.FormattingEnabled = true;
+            this.comboBoxBrand.Location = new System.Drawing.Point(69, 104);
+            this.comboBoxBrand.Name = "comboBoxBrand";
+            this.comboBoxBrand.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxBrand.TabIndex = 5;
+            this.comboBoxBrand.SelectedIndexChanged += new System.EventHandler(this.comboBoxBrand_SelectedIndexChanged_1);
+            // 
+            // comboBoxModel
+            // 
+            this.comboBoxModel.FormattingEnabled = true;
+            this.comboBoxModel.Location = new System.Drawing.Point(242, 104);
+            this.comboBoxModel.Name = "comboBoxModel";
+            this.comboBoxModel.Size = new System.Drawing.Size(286, 24);
+            this.comboBoxModel.TabIndex = 6;
+            this.comboBoxModel.SelectedIndexChanged += new System.EventHandler(this.comboBoxBrand_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(69, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Марка авто";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(249, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Модель авто";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(196, 316);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 9;
+            this.btnSave.Text = "save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButtonManual);
+            this.groupBox1.Controls.Add(this.radioButtonAutomatic);
+            this.groupBox1.Location = new System.Drawing.Point(95, 195);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(273, 80);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "4242";
+            // 
+            // radioButtonAutomatic
+            // 
+            this.radioButtonAutomatic.AutoSize = true;
+            this.radioButtonAutomatic.Location = new System.Drawing.Point(6, 30);
+            this.radioButtonAutomatic.Name = "radioButtonAutomatic";
+            this.radioButtonAutomatic.Size = new System.Drawing.Size(65, 20);
+            this.radioButtonAutomatic.TabIndex = 0;
+            this.radioButtonAutomatic.TabStop = true;
+            this.radioButtonAutomatic.Text = "AКПП";
+            this.radioButtonAutomatic.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonManual
+            // 
+            this.radioButtonManual.AutoSize = true;
+            this.radioButtonManual.Location = new System.Drawing.Point(147, 30);
+            this.radioButtonManual.Name = "radioButtonManual";
+            this.radioButtonManual.Size = new System.Drawing.Size(67, 20);
+            this.radioButtonManual.TabIndex = 1;
+            this.radioButtonManual.TabStop = true;
+            this.radioButtonManual.Text = "МКПП";
+            this.radioButtonManual.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBoxModel);
+            this.Controls.Add(this.comboBoxBrand);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxYear);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxYear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxBrand;
+        private System.Windows.Forms.ComboBox comboBoxModel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButtonManual;
+        private System.Windows.Forms.RadioButton radioButtonAutomatic;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ pz5/Pz5/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Pz5
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, List<string>> carModels = new Dictionary<string, List<string>>
+        {
+            {
+                "Audi", new List<string>
+                {
+                    "A3", "A4", "A6"
+                }
+            },
+            {
+                "BMW", new List<string>
+                {
+                    "3 Series", "5 Series", "7 Series"
+                }
+            },
+            {
+                "Mercedes-Benz", new List<string>
+                {
+                    "C-Class", "E-Class", "S-Class"
+                }
+            }
+        };
+
+        // Створюємо словник для збереження автомобілів за моделлю та роком
+        private Dictionary<string, List<(string, string)>> carList = new Dictionary<string, List<(string, string)>>
+        {
+            {
+                "A3", new List<(string, string)>
+                {
+                    ("Audi A3 2021", "Automatic"),
+                    ("Audi A3 2020", "Manual"),
+                    ("Audi A3 2019", "Automatic")
+                }
+            },
+            {
+                "A4", new List<(string, string)>
+                {
+                    ("Audi A4 2021", "Manual"),
+                    ("Audi A4 2020", "Automatic"),
+                    ("Audi A4 2019", "Automatic")
+                }
+            },
+            {
+                "A6", new List<(string, string)>
+                {
+                    ("Audi A6 2021", "Automatic"),
+                    ("Audi A6 2020", "Automatic"),
+                    ("Audi A6 2019", "Manual")
+                }
+            },
+            {
+                "3 Series", new List<(string, string)>
+                {
+                    ("BMW 3 Series 2021", "Manual"),
+                    ("BMW 3 Series 2020", "Automatic"),
+                    ("BMW 3 Series 2019", "Manual")
+                }
+            },
+            {
+                "5 Series", new List<(string, string)>
+                {
+                    ("BMW 5 Series 2021", "Automatic"),
+                    ("BMW 5 Series 2020", "Automatic"),
+                    ("BMW 5 Series 2019", "Manual")
+                }
+            },
+            {
+                "7 Series", new List<(string, string)>
+                {
+                    ("BMW 7 Series 2021", "Automatic"),
+                    ("BMW 7 Series 2020", "Automatic"),
+                    ("BMW 7 Series 2019", "Automatic")
+                }
+            },
+            {
+                "C-Class", new List<(string, string)>
+                {
+                    ("Mercedes-Benz C-Class 2021", "Manual"),
+                    ("Mercedes-Benz C-Class 2020", "Automatic"),
+                    ("Mercedes-Benz C-Class 2019", "Manual")
+                }
+            },
+            {
+                "E-Class", new List<(string, string)>
+                {
+                    ("Mercedes-Benz E-Class 2021", "Automatic"),
+                    ("Mercedes-Benz E-Class 2020", "Manual"),
+                    ("Mercedes-Benz E-Class 2019", "Automatic")
+                }
+            },
+            {
+                "S-Class", new List<(string, string)>
+                {
+                    ("Mercedes-Benz S-Class 2021", "Automatic"),
+                    ("Mercedes-Benz S-Class 2020", "Automatic"),
+                    ("Mercedes-Benz S-Class 2019", "Automatic")
+                }
+            }
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCarBrands();
+        }
+
+        private void InitializeCarBrands()
+        {
+            comboBoxBrand.Items.AddRange(carModels.Keys.ToArray());
+        }
+
+        private void comboBoxBrand_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedModel = comboBoxModel.SelectedItem.ToString();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string brand = comboBoxBrand.SelectedItem.ToString();
+            string model = comboBoxModel.SelectedItem?.ToString();
+            string year = textBoxYear.Text;
+            string transmission = radioButtonAutomatic.Checked ? "Automatic" : "Manual";
+
+            if (string.IsNullOrEmpty(model))
+            {
+                MessageBox.Show("Please select a car model.", "No Model Selected", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            List<(string, string)> availableCars = carList[model];
+            List<string> filteredCars = availableCars
+                .Where(car => car.Item1.Contains(year) && car.Item2 == transmission)
+                .Select(car => car.Item1)
+                .ToList();
+
+            if (filteredCars.Count == 0)
+            {
+                MessageBox.Show("There are no cars available for the selected model, year, and transmission.", "No Cars Found", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            string message = $"Available {transmission} cars for {brand} {model} in {year}:\n\n";
+            message += string.Join("\n", filteredCars);
+
+            MessageBox.Show(message, "Available Cars", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           //InitializeComponent();
+        }
+        private void comboBoxBrand_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            string selectedBrand = comboBoxBrand.SelectedItem.ToString();
+            comboBoxModel.Items.Clear();
+            comboBoxModel.Items.AddRange(carModels[selectedBrand].ToArray());
+        }
+    }
+}
